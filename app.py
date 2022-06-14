ec45384b02d58f047c684def217d6bfaa4481b09
edarakchiev <e_darakchiev@yahoo.com>
2022-06-14 10:33:25 +0300

    add mongoDB configuration

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models import Movie
 
 app = Flask(__name__)
 
-movies = [
-    {
-        "name": "The Shawshank Redemption",
-        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
-        "genres": ["Drama"]
-    },
-    {
-        "name": "The Godfather ",
-        "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
-        "genres": ["Crime", "Drama"]
-    }
-]
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/movie'
+}
+initialize_db(app)
 
 
 @app.route('/movies')
-def hello():
-    return jsonify(movies)
+def get_movies():
+    movies = Movie.objects().to_json()
+    return Response(movies, mimetype="application/json", status=200)
+
+
+@app.route('/movies/<movie_id>')
+def get_movie(movie_id):
+    movie = Movie.objects().get(id=movie_id).to_json()
+    return Response(movie, mimetype="application/json", status=200)
 
 
 @app.route('/movies', methods=['POST'])
 def add_movie():
-    movie = request.get_json()
-    movies.append(movie)
-    return {'id': len(movies)}, 200
+    body = request.get_json()
+    movie = Movie(**body).save()
+    movie_id = movie.id
+    return {'id': str(movie_id)}, 200
 
 
-@app.route('/movies/<int:index>', methods=['PUT'])
-def update_movie(index):
-    movie = request.get_json()
-    movies[index] = movie
-    return jsonify(movies[index]), 200
+@app.route('/movies/<movie_id>', methods=['PUT'])
+def update_movie(movie_id):
+    body = request.get_json()
+    Movie.objects.get(id=movie_id).update(**body)
+    return '', 200
 
 
-@app.route('/movies/<int:index>', methods=['DELETE'])
-def delete_movie(index):
-    movies.pop(index)
-    return 'None', 200
+@app.route('/movies/<movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+    Movie.objects.get(id=movie_id).delete()
+    return '', 200
 
 
 app.run()
